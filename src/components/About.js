4516ba8ddf0feca5a6dd4d7df11dd12d30b5783d
eddyvy <edwardyue93@gmail.com>
2021-06-24 12:33:50 +0200

    changed typography and added pictures

--- src/components/About.js
+++ src/components/About.js
@@ -4,18 +4,22 @@ export const About = () => {
     return (
         <div className="about__main" id="about">
             <h2>About Kloos</h2>
-            <p>
+            <p style={{ marginBottom: '50px'}}>
                 <br />
                 Raised in Palma de Mallorca (Spain) and currently residing in Budapest (Hungary), this young half-Spanish/half-Dutch DJ and producer has been able to perform in different venues along the Spanish Peninsula as well as the Hungarian capital.
                 <br />
+                <img src="https://scontent.fpmi1-1.fna.fbcdn.net/v/t1.6435-9/192255660_3951764478236804_7072694704252657118_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=XDSXIYKOSE0AX8D4i9j&_nc_ht=scontent.fpmi1-1.fna&oh=f0afa624a39d1c64f27f5e36b0345a78&oe=60D9A54A" alt="djkloos" style={{ width: '50vw', margin: '5px' }} />
                 <br />
                 In clubs like Fabrik, Sala Arena and La Riviera (Madrid), Corvin Club and Akvarium (Budapest) he has done what he loves most: to express himself through music.
                 <br />
+                <img src="https://scontent.fpmi1-1.fna.fbcdn.net/v/t31.18172-8/12183009_908573419222607_3507700548034818121_o.jpg?_nc_cat=111&ccb=1-3&_nc_sid=cdbe9c&_nc_ohc=J9eNMxM88awAX9CeCmg&_nc_ht=scontent.fpmi1-1.fna&oh=f80fba2bfa90a7bd0e8f19bf25838e9b&oe=60DA30B6" alt="djkloos" style={{ width: '50vw', margin: '5px' }} />
                 <br />
                 In terms of personal style, his genre domain goes from Techno to Progressive House and his sets are known to be carefully prepared, always offering variety and looking to surprise the crowd. An excellent musical selection, creative mashups and a great connection with the audience make of his performance a journey to remember.
                 <br />
+                <img src="https://scontent.fpmi1-1.fna.fbcdn.net/v/t1.18169-9/30124562_1651562938256981_6875911024825135317_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=730e14&_nc_ohc=Lg-jGcaDaecAX-YIyhA&_nc_ht=scontent.fpmi1-1.fna&oh=b1f224018c8edc9836f19ecb4d3d75b4&oe=60D8915E" alt="djkloos" style={{ width: '50vw', margin: '5px' }} />
                 <br />
                 Production-wise, several releases in different genres and labels like Jose de Mara's Arcadia Music, or Metha's BeMassive, have gathered support by artists like Tiësto, Chris Avantgarde, Artche, Dim3nsion, Low Blow and Dissonance, to name a few.
+                <br/>
             </p>
         </div>
     )
